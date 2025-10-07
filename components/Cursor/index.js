20cbae096a534c1f2565f8906b09c119de120394
AkaTFL <coldway3108@gmail.com>
2025-10-07 14:17:06 +0200

    Big Delete
    Deleting parts of the original portfolio that doesn't fit with my objectives

---
+++ components/Cursor/index.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import AnimatedCursor from "react-animated-cursor";
+import { useTheme } from "next-themes";
+
+const Cursor = () => {
+  const theme = useTheme();
+  const [mount, setMount] = useState();
+
+  const getCusomColor = () => {
+    if (theme.theme === "dark") {
+      return "#fff";
+    } else if (theme.theme === "light") {
+      return "#000";
+    }
+  };
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+  return (
+    <>
+      {mount && (
+        <AnimatedCursor
+          targets={[".link"]}
+          customClass="custom-cursor"
+          dimensions={30}
+          fill={getCusomColor()}
+          smoothness={{
+            movement: 0.2,
+            scale: 0.1,
+            opacity: 0.2,
+          }}
+          targetOpacity={0.5}
+          targetScale={2}
+        />
+      )}
+    </>
+  );
+};
+
+export default Cursor;
